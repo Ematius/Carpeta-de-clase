245bf34e9d601493d2475ee2c43a313b208b2dbd
Emad Kadyear <eksaber87@gmail.com>
2024-12-09 19:57:50 +0100

    09-12-24-casa

--- JS/tools.js
+++ JS/tools.js
@@ -10,7 +10,26 @@ export function countWords(sentence = '') {
     //Esto es programación defensiva, escribiendo primero lo que no se acepta
     if (typeof sentence !== 'string' || sentence === '')
         return 'Valor Invalido';
-    
+
     const words = sentence.split(' ');
     return words.length;
 }
+
+export function randomPasswords(pass) {
+    if (typeof pass !== 'number')
+        return 'Por favor introduce la longitud de la contraseña random que quieres';
+
+    let randomPass = '';
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < pass; i++) {
+        let random = Math.round(Math.random() * characters.length);
+        randomPass += characters[random];
+    }
+    return randomPass;
+}
+
+console.log(randomPasswords(10));
+
+console.log(typeof 42);
