ab54e32330b9774f0aec04cc15cc9cd6c53164a3
ematius <eksaber87@gmail.com>
2024-12-26 16:36:58 +0100

    update

--- JS/christmas-challenges/01_GoT/assets/components/cards.js
+++ JS/christmas-challenges/01_GoT/assets/components/cards.js
@@ -1,17 +1,17 @@
 import { render } from "./render.js";
 
-function createCard(){
+function createCard(character){
     const selector = '.characters-list';
     const position = 'beforeend';
-    const template = /*html*/` 
+    const template = /*html*/ ` 
     <li class="character col">
         <div class="card character__card">
-          <img src="img/no-one.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <img src="${character.image}" alt="${character.name} y ${character.family}" class="character__picture card-img-top" />
           <div class="card-body">
-            <h2 class="character__name card-title h4">Nombre y familia</h2>
+            <h2 class="character__name card-title h4">"${character.name} y ${character.family}"</h2>
             <div class="character__info">
               <ul class="list-unstyled">
-                <li>Edad: X años</li>
+                <li>Edad: ${character.age} años</li>
                 <li>
                   Estado:
                   <i class="fas fa-thumbs-down"></i>
@@ -21,9 +21,10 @@ function createCard(){
             </div>
             <div class="character__overlay">
               <ul class="list-unstyled">
-                <li>Años de reinado: X</li>
-                <li>Arma: XXX</li>
-                <li>Destreza: X</li>
+                ${character.reignYears ? `<li>Años de reinado: ${character.reignYears}</li>` : ''}
+                ${character.weapon ? `<li>Arma: ${character.weapon}</li>` : ''}
+                ${character.skillLevel ? `<li>Destreza: ${character.skillLevel}</li>` : ''}
+                <li>Mensaje: ${character.message}</li>
                 <li>Peloteo: X</li>
                 <li>Asesora a: X</li>
                 <li>Sirve a: X</li>
@@ -39,5 +40,19 @@ function createCard(){
       </li>
     `;
     render(selector, position, template);
+
 }
-createCard();
+
+async function getCharacters(){
+  try{
+    const response = await fetch('../db.json');
+    const data = await response.json();
+    data.characters.forEach( character => {
+      createCard(character);
+    });
+  } catch(error){
+    console.error('Error:', error);
+  }
+}
+
+getCharacters();
