bc8046417e2b388c2f7dd1be85da7fff680f25ec
Emad <eksaber87@gmail.com>
2025-03-31 14:00:32 +0200

    31/03

--- Tercer-Modulo/testing-front/src/components/character.test.ts
+++ Tercer-Modulo/testing-front/src/components/character.test.ts
@@ -1,16 +1,21 @@
+import { createCharacter } from './character.js';
+import { render } from './base.js';
+import { Character } from '../types/character.js';
+
+const mockCharacter: Character = {} as Character;
+
 
 describe('Given render function', () => {
     describe('When called with valid parameters', () => {
         test('Then it should return a valid HTML element', () => {
             // Arrange
             const selector = 'body';
-            const position:InsertPosition = 'beforeend';
+            const position: InsertPosition = 'beforeend';
             const template = '<div>test</div>';
             // Act
             const element = render(selector, position, template);
             // Assert
-            expect(element).toBeInstanceOf(HTMLElement);
-
+            expect(createCharacter).toBeCalledWith(selector, position, mockCharacter);
         });
     });
 });
